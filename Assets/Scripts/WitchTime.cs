1286ab284bd81528c970db48295f218504f8b68f
Nicolas07n <nicolasmg4012@gmail.com>
2025-02-21 17:14:45 +0100

    He Hecho el Witch y me hace el slow , lo que no he conseguido es lo del sonido lo demas esta

--- Assets/Scripts/WitchTime.cs
+++ Assets/Scripts/WitchTime.cs
@@ -4,9 +4,38 @@ using UnityEngine;
 
 public class WitchTime : MonoBehaviour
 {
+    
+
+
+    void Start()
+    {
+        
+    }
     // Update is called once per frame
     void Update()
     {
 
+        if (Input.GetMouseButtonDown(1)) 
+        {
+            StartCoroutine(ActivateWitchTime());
+        }
+       
     }
-}
+    IEnumerator ActivateWitchTime()
+    {
+
+        Time.timeScale = 0.25f;
+
+
+
+        yield return new WaitForSeconds(1.0f);
+
+
+
+
+
+        Time.timeScale = 1f;
+
+
+
+    }    }
